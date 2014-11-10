f5038fc5904c102970737c97f8d09abe14618dc0
Thaichor Seng <thaichor@gmail.com>
2014-11-10 10:29:01 +0700

    fix articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,18 @@
 class ArticlesController < ApplicationController
 
-  # load_and_authorize_resource
+  before_action :authenticate_user!,    only: [:new, :create, :edit, :update, :delete]
+  before_action :set_user_new_article,  only: [:new, :create]
+  before_action :set_user_article,      only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.all
-    authorize! :index, @articles
   end
 
   def new
-    @article = Article.new
-    authorize! :new, @article
   end
 
   def create
-    @article = current_user.articles.build article_params
-    authorize! :create, @article
-
+    @article.assign_attributes(article_params)
     if @article.save
       redirect_to @article
     else
@@ -25,29 +22,22 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-    authorize! :show, @articles
   end
 
   def edit
-    @article = Article.find(params[:id])
-    authorize! :show, @articles
   end
 
   def update
-    @article = current_user.articles.find params[:id]
-    authorize! :show, @articles
-
-    if @article.update_attributes(article_params)
+    @article.assign_attributes(article_params)
+    if @article.save
       redirect_to @article
     else
       render :edit
     end
   end
 
-  def delete
-    @article = Article.find(params[:id])
-    authorize! :show, @articles
-    @article.destroy
+  def destroy
+    @article.destroy!
 
     redirect_to articles_url
   end
@@ -55,5 +45,15 @@ class ArticlesController < ApplicationController
   private
     def article_params
       @article_params ||= params.require(:article).permit(:title, :content)
+    end
+
+    def set_user_article
+      @article = current_user.articles.find(params[:id])
+      authorize! :update, @articles
+    end
+
+    def set_user_new_article
+      @article = current_user.articles.build
+      authorize! :create, @article
     end
 end
